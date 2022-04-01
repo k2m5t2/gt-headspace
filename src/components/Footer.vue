db68b95c96a07147284ac22b154c394a6725e324
k2m5t2 <yhjc2692@gmail.com>
2022-03-31 23:55:33 -0400

    basically done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,23 +12,12 @@ const toggleLocales = () => {
 
 <template>
   <nav text-xl mt-6>
-    <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
-      <div i-carbon-campsite />
-    </router-link>
 
     <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
-    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
-      <div i-carbon-language />
-    </a>
-
-    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
-      <div i-carbon-dicom-overlay />
-    </router-link>
-
-    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/k2m5t2/gt-headspace" target="_blank" title="GitHub">
       <div i-carbon-logo-github />
     </a>
   </nav>
---
+++ src/pages/index.md
@@ -0,0 +1,16 @@
+---
+title: About
+---
+
+# **Headspace**
+
+Headspace is a guided meditation app with over 65 million users. Georgia Tech offers full access to Headspace for its students. The sign-up link is quite hard to find, though. [Here](https://work.headspace.com/usggeorgiatech/member-enroll) is the link.
+
+[![image](hs_banner.png)](https://work.headspace.com/usggeorgiatech/member-enroll)
+<figcaption align = "center"><b>You can click the image too</b></figcaption>
+
+
+# **Info**
+
+- [Original page](https://gtwellnesshub.com/personal-guide-to-health-happiness/)
+- [Related (reddit)](https://www.reddit.com/r/gatech/comments/ptopi7/icymi_georgia_tech_has_purchased_a_headspace/)
