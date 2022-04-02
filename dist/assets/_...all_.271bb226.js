c42736cc35357e83169950069e3516076bcd437a
k2m5t2 <yhjc2692@gmail.com>
2022-04-02 00:53:28 -0400

    Ignore CNAME

---
+++ dist/assets/_...all_.271bb226.js
@@ -0,0 +1,1 @@
+import{d as n,b as o,o as s,c as a,t as c,e as r}from"./index.07edad2f.js";var t={};const f=n({setup(p){const{t:e}=o();return(u,i)=>(s(),a("div",null,c(r(e)("not-found")),1))}});typeof t=="function"&&t(f);export{f as default};
